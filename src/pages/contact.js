391f5e24f8325470d05378957318067af86ab296
Jennifer Chow <jennego@gmail.com>
2026-01-28 17:24:26 -0800

    adjust width

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import ResponsiveAppBar from "../components/menu";
 import Footer from "../components/footer";
+import { Container } from "@mui/material";
 
 const ContactPage = () => {
   return (
     <div>
       <ResponsiveAppBar />
+      <Container></Container>
       <Footer />
     </div>
   );
